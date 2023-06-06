683bbfc4c709c768b75dfc79b980691a16df9596
yong.tan <Yong.tan@bangcle.com>
2023-06-06 15:05:32 +0800

    修改内容

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-      ? '/study/'
-      : '/',
+  base: '/study/',
   plugins: [vue()],
 })
